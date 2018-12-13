0273a7af8a417d963d00b89e4f25d1f537c338f7
Paul MacDougall <paul.macdougall@cigna.com>
2018-12-13 08:09:56 -0800

    Had issues with berks using the ruby code for version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,10 @@ maintainer_email 'gba@onbeep.com'
 license          'The MIT License (MIT)'
 description      'SSL key & certificate storage in chef-vault.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '1.2.0'
+# seems that berks install" does not like this when using git reposotory in Berksfile 
+# version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '1.2.0'
+# keeping version simple for our fork
+version '1.2.0'
 
 supports 'ubuntu'
 
